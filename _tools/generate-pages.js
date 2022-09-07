75cdb10e01cf4986bb418dd2d3a9421966294d18
Dominique Hazael-Massieux <dom@w3.org>
2022-09-07 10:23:42 +0200

    Adapt to links structure change

--- _tools/generate-pages.js
+++ _tools/generate-pages.js
@@ -392,7 +392,7 @@ ${content}`);
   // await fs.writeFile('terms.json', JSON.stringify([...termIndex.entries()], null, 2));
   for (const spec of results) {
     const fullLinks = Object.keys(spec.links || {}).reduce((acc, link) => {
-      acc.push(spec.links[link].anchors.map(frag => link + "#" + frag));
+      acc.push((spec.links[link].anchors || []).map(frag => link + "#" + frag));
       return acc;
     }, []).flat();
     for (const link of fullLinks) {
